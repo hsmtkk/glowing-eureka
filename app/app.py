a5aba13590caba21a54015dda65154a837819c56
hsmtkk <hsmtkk@gmail.com>
2023-03-10 09:59:17 +0900

    use flask logger

--- app/app.py
+++ app/app.py
@@ -6,19 +6,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-logger = logging.getLogger(__name__)
-
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    logger.info("query string: " + request.query_string)
-    logger.info(json.dumps({"query_string": request.query_string}))
+
+    # log
+    query_str = request.query_string.decode()
+    app.logger.info("query string: " + query_str)
+    app.logger.info(json.dumps({"query_string": query_str}))
+
     return "Hello {}!".format(name)
 
 if __name__ == "__main__":
-    if "DEBUG" in os.environ:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.WARN)
     port=int(os.environ.get("PORT", "8080"))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    if "DEBUG" in os.environ:
+        logging.basicConfig(level=logging.DEBUG)
+        app.run(debug=True, host="0.0.0.0", port=port)
+    else:
+        app.run(host="0.0.0.0", port=port)
