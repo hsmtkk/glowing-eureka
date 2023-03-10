e52b59df14fed5be6f49726edad318fb7e1c523c
hsmtkk <hsmtkk@gmail.com>
2023-03-10 10:10:20 +0900

    enable Google Cloud Profiler

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 
+import googlecloudprofiler
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -19,8 +21,15 @@ def hello_world():
 
 if __name__ == "__main__":
     port=int(os.environ.get("PORT", "8080"))
+
+    debug=False
     if "DEBUG" in os.environ:
+        debug=True
         logging.basicConfig(level=logging.DEBUG)
-        app.run(debug=True, host="0.0.0.0", port=port)
-    else:
-        app.run(host="0.0.0.0", port=port)
+
+    try:
+        googlecloudprofiler.start(service='example')
+    except Exception as e:
+        app.logger.error("failed to start Google Cloud Profiler: " + str(e))
+
+    app.run(debug=debug, host="0.0.0.0", port=port)
